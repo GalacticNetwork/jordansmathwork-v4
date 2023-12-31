e53a42b6d3ee5bebfed3ee3b1ec2cb79c497efcb
GalacticNetwork <114846211+GalacticNetwork@users.noreply.github.com>
2023-12-31 23:53:52 +0000

    added kahoot theme and random game picker

---
+++ assets/scripts/random.js
@@ -0,0 +1,40 @@
+function randomLink() {
+  var links = [
+    "/gs/files.html?file=fnacr&type=local",
+    "/gs/files.html?file=fruit-ninja&type=local",
+"/gs/files.html?file=four-colors&type=local",
+"/gs/files.html?file=bitlife&type=local",
+"/gs/files.html?file=10-minutes-till-dawn&type=3kh0",
+"/gs/files.html?file=granny&type=local",
+"/gs/files.html?file=justfall.lol&type=local",
+"/gs/files.html?file=tiq-book&type=local",
+"/gs/files.html?file=snake&type=local",
+"/gs/files.html?file=superhot&type=local",
+"/gs/files.html?file=tanuki-sunset&type=local",
+"/gs/files.html?file=drift-boss&type=local",
+"/gs/files.html?file=drift-hunters&type=local",
+"/gs/files.html?file=fnaf/1&type=local",
+"/gs/files.html?file=getting-over-it&type=local",
+"/gs/files.html?file=subway-surfers&type=3kh0",
+"/gs/files.html?file=stumble&type=local",
+"/gs/files.html?file=pvz&type=local",
+"/gs/files.html?file=run-3&type=local",
+"/gs/files.html?file=snowball.io&type=local",
+"/gs/files.html?file=sm64&type=local",
+"/gs/files.html?file=shell-shockers&type=local",
+"/gs/files.html?file=ovo&type=3kh0",
+"/gs/files.html?file=fortnite&type=local",
+"/gs/files.html?file=roblox&type=local",
+"/gs/files.html?file=angry-birds&type=local",
+"/gs/files.html?file=among-us&type=local",
+"/gs/files.html?file=oldvsnew&type=local",
+"/gs/files.html?file=1v1-battle&type=local",
+
+
+  ];
+  var randomIndex = Math.floor(Math.random() * links.length);
+  window.open(links[randomIndex]);
+}
+
+
+
