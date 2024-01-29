54e5dd93bef3615350f6e41b329a9a9e60866ba5
GalacticNetwork <114846211+GalacticNetwork@users.noreply.github.com>
2024-01-29 17:46:11 +0000

    7 new games

--- assets/scripts/random.js
+++ assets/scripts/random.js
@@ -11,9 +11,9 @@ function randomLink() {
 "/gs/files.html?file=snake&type=local",
 "/gs/files.html?file=superhot&type=local",
 "/gs/files.html?file=tanuki-sunset&type=local",
-"/gs/files.html?file=drift-boss&type=local",
-"/gs/files.html?file=drift-hunters&type=local",
-"/gs/files.html?file=fnaf/1&type=local",
+"/gs/files.html?file=drift-boss/inde.html&type=local",
+"/gs/files.html?file=drift-hunters/inde.html&type=local",
+"/gs/files.html?file=fnaf/1/2.html&type=local",
 "/gs/files.html?file=getting-over-it&type=local",
 "/gs/files.html?file=subway-surfers&type=3kh0",
 "/gs/files.html?file=stumble&type=local",
@@ -40,20 +40,22 @@ function randomLink() {
     "/gs/files.html?file=fire-boy-and-water-girl&type=local",
     "/gs/files.html?file=FNF&type=local",
     "/gs/files.html?file=football-legends&type=local",
-    "/gs/files.html?file=fnaf/3&type=local",
+    "/gs/files.html?file=fnaf/3/2.html&type=local",
     "/gs/files.html?file=four-colors&type=local",
     "/gs/files.html?file=minecraft-classic&type=local",
     "/gs/files.html?file=osu&type=local",
     "/gs/files.html?file=neal.fun&type=local",
     "/gs/files.html?file=parking-fury/3&type=local",
-    "/gs/files.html?file=pokemon/emerald&type=local",
+    "/gs/files.html?file=pokemon/emerald/inde.html&type=local",
     "/gs/files.html?file=slope-2&type=local",
     "/gs/files.html?file=snake&type=local",
     "/gs/files.html?file=snowrider3d&type=local",
 
   ];
   var randomIndex = Math.floor(Math.random() * links.length);
-  window.open(links[randomIndex]);
+
+
+  window.location.href = links[randomIndex];
 }
 
 
